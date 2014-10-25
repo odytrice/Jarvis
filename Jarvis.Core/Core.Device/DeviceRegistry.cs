c2ec4d8a851c14f6747be0f1e8ef0803fc843590
Stanley Damasus <stanley.damasus@yahoo.com>
2014-10-25 00:31:58 -0700

    Core classes built (version 0.0.0)

--- Jarvis.Core/Core.Device/DeviceRegistry.cs
+++ Jarvis.Core/Core.Device/DeviceRegistry.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jarvis.Core
+namespace Jarvis.Core.Device
 {
-    public class Class1
+    public class DeviceRegistry
     {
+        IEnumerable<IDevice> Devices { get; set; }
     }
 }
