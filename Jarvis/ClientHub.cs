ce70b8d55fb38714031e46c0d3770cbc945ea09e
Ody Mbegbu <odytrice@gmail.com>
2014-10-25 18:03:08 +0100

    Initial Load

---
+++ Jarvis/ClientHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Jarvis
+{
+    public class ClientHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ Jarvis/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(Jarvis.Startup))]
+
+namespace Jarvis
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
+            hubConfiguration.EnableJSONP = true;
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+}
