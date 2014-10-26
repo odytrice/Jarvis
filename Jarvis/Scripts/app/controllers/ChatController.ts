6e2369474705c49d4201f9c441633d1e59b60c1e
Jomeno <jomeno.vona@gmail.com>
2014-10-26 02:29:09 +0100

    Added Angular

---
+++ Jarvis/Scripts/app/controllers/ChatController.ts
@@ -0,0 +1,21 @@
+﻿app.controller("ChatController", function ($scope) {
+
+    var models = {
+        Message: {
+            sender: 'You',
+            body: 'Hi Jarvis'
+        }
+    };
+
+    var sendMessage = function () {
+
+        console.log('good');
+
+    }
+
+    angular.extend($scope, {
+        Message : models.Message,
+        SendMessage: sendMessage
+    });
+
+})
